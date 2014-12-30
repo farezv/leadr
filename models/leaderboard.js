a4c1ceff015fb5c8ad9c1cce9e49fcfc49c9d530
Farez Vadsaria <farezvad@gmail.com>
2014-12-30 01:44:43 -0800

    No need for leaderboard model

--- models/leaderboard.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var LeaderboardSchema = new Schema({
-    name: String,
-    stat_list: String
-});
-
-module.exports = mongoose.model('Leaderboard', LeaderboardSchema);
